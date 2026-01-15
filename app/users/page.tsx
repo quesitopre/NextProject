862b83b74fe99620180d102640eae51617a3c1ec
quesitopre <citlallimartinez122@gmail.com>
2026-01-15 08:58:29 -0800

    revalidate data-every 10sec

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+interface User{
+    id: number;
+    name: string;
+}
 
-const UsersPage = () => {
+const UsersPage = async () => { 
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',
+         { next: { revalidate: 10} }); //refresh data every 10sec onn backend.
+    const users: User[] = await res.json(); // await the response from the user 
+
   return (
-    <div>UsersPage</div>
+    <> 
+        <h1>Users</h1>
+        <p>{new Date().toLocaleTimeString()}</p> {/* shows the date when the page was rendered on the server */}
+        <ul>
+            {users.map(user =><li key={user.id}>{user.name}</li>)}
+        </ul>
+    </>
+
   )
 }
 
